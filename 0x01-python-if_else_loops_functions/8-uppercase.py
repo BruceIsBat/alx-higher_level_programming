c230dc5022b20429f7b74f818df11f825a260e8f
BruceIsBat <awoteduibrahim@gmail.com>
2023-08-10 18:29:08 +0000

    high level language

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def uppercase(str):
+    result = ''
+    for char in s:
+        if 'a' <= char <= 'z':
+            result += chr(ord(char) - ord('a') + ord('A'))
+        else:
+            result += char
+    print(result)
