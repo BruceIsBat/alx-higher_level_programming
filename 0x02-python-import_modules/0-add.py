cdde3c153725b5d8bf07fa06e47bcd21aac7ea27
BruceIsBat <awoteduibrahim@gmail.com>
2023-08-18 17:56:26 +0000

    highlevel

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -2,7 +2,7 @@
 if __name__ == "__main__":
 
     from add_0 import add
-    value1 = 1
-    value2 = 2
+    a = 1
+    b = 2
     ADD = add(value1, value2)
     print("{} + {} = {}".format(value1, value2, ADD))
