40f9240b7555cd4f223de76ecab8b1c80c27d336
BruceIsBat <awoteduibrahim@gmail.com>
2023-08-18 17:53:56 +0000

    highlevel

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-if __name__ == __main__:
+if __name__ == "__main__":
 
-from add_0 import add
-value1 = 1
-value2 = 2
-ADD = add(value1, value2)
-print("{} + {} = {}".format(value1, value2, ADD)
+    from add_0 import add
+    value1 = 1
+    value2 = 2
+    ADD = add(value1, value2)
+    print("{} + {} = {}".format(value1, value2, ADD))
