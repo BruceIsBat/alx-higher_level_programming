d9e9bfc31ce85cf08d9706e4641f09733273ac1a
BruceIsBat <awoteduibrahim@gmail.com>
2023-08-18 17:46:02 +0000

    high level

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 if __name__ = __main__:
+
 from add_0 import add
-a = 1
-b = 2
-ad = add(a, b)
-print(f"{a} + {b} = {ad}")
+value1 = 1
+value2 = 2
+ADD = add(value1, value2)
+print("{} + {} = {}".format(value1, value2, ADD)
