d188dc3babecf06f42bbeeced3aab607dce83bef
BruceIsBat <awoteduibrahim@gmail.com>
2023-08-20 10:19:27 +0000

    highlevel

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -2,13 +2,13 @@
 import dis
 
 
-def print_defined_names():
-    code = __import__("hidden_4").__code__
-    names = code.co_names
-    filtered_names = sorted(filter(lambda name: not name.startswith('__'), names))
-    for name in filtered_names:
-        print(name)
+def print_defined_names(module_name):
+    module = __import__(module_name)
+    names = sorted(dir(module))
+    for name in names:
+        if not name.startswith('__'):
+            print(name)
 
 
 if __name__ == "__main__":
-    print_defined_names()
+    print_defined_names("hidden_4")
