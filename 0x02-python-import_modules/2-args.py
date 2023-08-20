1c12d9ded1b19966338645f56b7f1e27738e65df
BruceIsBat <awoteduibrahim@gmail.com>
2023-08-20 08:07:52 +0000

    highlevel permission

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 import sys
-def main():
 
+
+def main():
     if len(sys.argv) == 1:
         print("{} arguments.".format(len(sys.argv)-1))
     elif len(sys.argv) == 2:
@@ -11,6 +12,7 @@ def main():
         print("{} arguments:".format(len(sys.argv)-1))
         for i in range(1, len(sys.argv)):
             print("{}: {}".format(i, sys.argv[i]))
+
 
 if __name__ == "__main__":
     main()
