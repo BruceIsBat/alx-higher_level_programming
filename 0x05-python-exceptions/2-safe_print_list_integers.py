7e021194a886915201929e44f1d02025962d2d71
BruceIsBat <awoteduibrahim@gmail.com>
2023-08-28 15:52:54 +0000

    Error handling

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    pt = 0
+
+    try:
+        for i in range(x):
+            if i < x:
+                if isinstance(my_list[i], int):
+                    print("{:d}".format(my_list[i]), end=(""))
+                    pt += 1
+        print()      
+    except:
+        raise
+
+    return(pt) 
