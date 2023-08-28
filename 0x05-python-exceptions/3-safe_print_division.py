13091c38f452f32f0ef2d67bda6db426c36e7a72
BruceIsBat <awoteduibrahim@gmail.com>
2023-08-28 23:32:42 +0000

    element

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -7,11 +7,7 @@ def safe_print_division(a, b):
         print("Inside result: None")
     finally:
         if 'result' in locals():
-            return("{}".format(result))
+            return(result)
         else:
             result = None
-            return("{}".format(result))
-
-
-if __name__ = "__main__":
-   safe_print_division(a, b)
+            return(result)
