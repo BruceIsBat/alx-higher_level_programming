4da99d8468e1a0938d79f95e7f338f59aaba0db3
BruceIsBat <awoteduibrahim@gmail.com>
2023-08-29 02:10:05 +0000

    well

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -2,10 +2,10 @@
 def uniq_add(my_list=[]):
     unique_set = set()
     result = 0
-    
+
     for num in my_list:
         if num not in unique_set:
             result += num
             unique_set.add(num)
-    
+
     return result
