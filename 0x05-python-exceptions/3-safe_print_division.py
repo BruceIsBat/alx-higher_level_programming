0c5704a5cd39552da68cfbeb666288c984f818b4
BruceIsBat <awoteduibrahim@gmail.com>
2023-08-29 10:29:50 +0000

    second deadline

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -3,11 +3,11 @@ def safe_print_division(a, b):
     try:
         result = a/b
         print("Inside result: {}".format(result))
-    except:
+    except ZeroDivisionError:
         print("Inside result: None")
     finally:
         if 'result' in locals():
-            return(result)
+            return (result)
         else:
             result = None
-            return(result)
+            return (result)
