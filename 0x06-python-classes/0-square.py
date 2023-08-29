ab7aeded3684f89ff263942e93ad17d9348252ba
BruceIsBat <awoteduibrahim@gmail.com>
2023-08-29 12:37:41 +0000

    classes and objects

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+class Square:
+    """
+    This is an empty class with no attributes or methods.
+
+    This class can be used as a starting point for creating new classes
+    or as a placeholder for future development.
+
+    Example usage:
+        emp = Square()
+
+    Attributes:
+        None
+
+    Methods:
+        None
+    """
+    pass
