4e41c2cd07d880a2c138229c3dd4ff3d5dc85d63
BruceIsBat <awoteduibrahim@gmail.com>
2023-09-11 14:46:47 +0000

    fixed pycodestyle validiation error

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """ This module converts two argument to int then add them
 """
+
+
 def add_integer(a, b=98):
     """ The function to add two floats or integers
 
@@ -12,12 +14,15 @@ def add_integer(a, b=98):
         The return is the sum of the two
     """
 
+
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
     elif not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
     else:
         return int(a + b)
+
+
 if __name__ == "__main__":
     import doctest
     doctest.testfile("tests/0-add_integer.txt")
