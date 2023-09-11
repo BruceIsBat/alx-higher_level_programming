1a5811ae15149e1a310cec8d87828d8f36f95db0
BruceIsBat <awoteduibrahim@gmail.com>
2023-09-11 16:30:02 +0000

    testing a matrix division

---
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""This module contains a code to get a matrix and divisor
+"""
+def matrix_divided(matrix, div):
+    """This function divides a matrix and returns a new matrix
+
+    Args:
+        matrx: gets the matrix to be divided
+        div: the divisor
+
+    Returns:
+        return a new divided matrix
+    """
+    try:
+        new_matrix = []
+        if not isinstance(div, (int, float)):
+            raise TypeError("div must be a number")
+        elif div == 0:
+            raise ZeroDivisionError("division by zero")
+        for mat_list in range(len(matrix)):
+            new_index_list = []
+            if len(matrix[mat_list]) != len(matrix[mat_list-1]):
+                raise TypeError("Each row of the matrix must have the same size")
+            new_matrix.append(new_index_list)
+            for element in matrix[mat_list]:
+                if not isinstance(element, (int, float)):
+                    raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+                mat_div = round(element/div, 2)
+                new_index_list.append(mat_div)
+        return new_matrix
+    except TypeError as var:
+        print(var)
+    except ZeroDivisionError as e:
+        print(e)
+    except TypeError as a:
+        print(a)
+
+
+if __name__ == "__main__":
+    import doctest
+    doctest.textfile("tests/2-matrix_divided.txt")
