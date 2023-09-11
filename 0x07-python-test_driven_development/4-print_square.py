14b37012697b57ca5d7d8de5d4a38b4c8e28423e
BruceIsBat <awoteduibrahim@gmail.com>
2023-09-11 21:21:33 +0000

    square

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,34 @@
+"""This module prints a square with the character #
+"""
+
+
+def print_square(size):
+    """ function that prints a square with the character #
+
+    Args:
+        size: integer value
+
+    Return:
+        returns none
+    """
+    try:
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        elif isinstance(size, float) and size < 0:
+            raise TypeError("size must be an integer")
+        else:
+            for first in range(size):
+                for second in range(size):
+                    print("#", end='')
+                print()
+    except TypeError as e:
+        print(e)
+    except ValueError as r:
+        print(r)
+
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testfile("tests/4-print_square.txt")
