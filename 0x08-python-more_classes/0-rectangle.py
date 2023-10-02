4f9f889a79c403d871d68c5610795e4d10f2cab9
BruceIsBat <awoteduibrahim@gmail.com>
2023-10-02 22:39:09 +0000

    added

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" Mimics a Rectangle """
+
+
+class Rectangle:
+    """"
+    This class represents a rectangle shape.
+
+    Attributes:
+    None
+
+    Methods:
+    None
+    """
+    pass
