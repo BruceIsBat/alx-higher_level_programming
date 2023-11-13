349ff37b1f109f1aea0c80812404b119629bfe49
BruceIsBat <awoteduibrahim@gmail.com>
2023-11-13 10:37:11 -0800

    updated

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+const { argv } = require('process');
+if (argv[2] > 0) {
+  for (let i = 0; i < argv[2]; i++ ) {
+    console.log('C is fun');
+  }
+} else if (argv[2] < 0) {
+} else {
+  console.log('Missing number of occurrences');
+}
