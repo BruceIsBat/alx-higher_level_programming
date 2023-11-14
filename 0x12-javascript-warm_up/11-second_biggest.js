e8ccde354bf72d410dcc28ea6bd146b825e85ff2
BruceIsBat <awoteduibrahim@gmail.com>
2023-11-14 05:41:43 -0800

    updated

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 const { argv } = require('process');
+let max = argv[0];
 if (argv.length <= 3) {
-  console.log(0);
+  console.log('0');
 } else {
-  let max = argv[0];
   for (let i = 0; i < argv.length; i++) {
     if (argv[i] > max) {
       max = argv[i];
