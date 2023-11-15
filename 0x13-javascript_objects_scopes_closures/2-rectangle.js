08d690ac8963f479a5129e496605408d6278ef15
BruceIsBat <awoteduibrahim@gmail.com>
2023-11-15 09:23:54 -0800

    updated the files

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -2,7 +2,7 @@
 class Rectangle {
   constructor (w, h) {
     if (!parseInt(w) || !parseInt(h) || w < 1 || h < 1) {
-
+      return null;
     } else {
       this.width = w;
       this.height = h;
--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -2,7 +2,7 @@
 class Rectangle {
   constructor (w, h) {
     if (!parseInt(w) || !parseInt(h) || w < 1 || h < 1) {
-      null;
+      return null;
     } else {
       this.width = w;
       this.height = h;
