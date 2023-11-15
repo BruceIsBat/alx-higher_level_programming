509b4e42f47e534cdd8a71a2702ccedcba7ecde8
BruceIsBat <awoteduibrahim@gmail.com>
2023-11-15 08:10:49 -0800

    updated the file

--- 0x13-javascript_objects_scopes_closures/2-main.js
+++ 0x13-javascript_objects_scopes_closures/2-main.js
@@ -6,7 +6,7 @@ console.log(r1);
 console.log(r1.width);
 console.log(r1.height);
 
-const r2 = new Rectangle(2, -3);
+const r2 = new Rectangle(3, -3);
 console.log(r2);
 console.log(r2.width);
 console.log(r2.height);
--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,11 +1,12 @@
 #!/usr/bin/node
 class Rectangle {
   constructor (w, h) {
-    if (!Number.isInteger(w) || !Number.isInteger(h) || w < 1 || h < 1) {
+    if (!parseInt(w) || !parseInt(h) || w < 1 || h < 1) {
       return {};
+    } else {
+      this.width = w;
+      this.height = h;
     }
-    this.width = w;
-    this.height = h;
   }
 }
 module.exports = Rectangle;
