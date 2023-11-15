fe92d8f3a51ca39ccb0279d9f93be6aefd493740
BruceIsBat <awoteduibrahim@gmail.com>
2023-11-15 09:21:48 -0800

    updated

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,27 @@
+#!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    if (!parseInt(w) || !parseInt(h) || w < 1 || h < 1) {
+      return null;
+    } else {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      console.log('X'.repeat(this.width));
+    }
+  }
+
+  rotate () {
+    [this.height, this.width] = [this.width, this.height];
+  }
+
+  double () {
+    this.height *= 2;
+    this.width *= 2;
+  }
+}
+module.exports = Rectangle;
