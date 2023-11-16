46e804395a4b5ba695cebbd4cff05b28e60dc44e
BruceIsBat <awoteduibrahim@gmail.com>
2023-11-16 07:47:29 -0800

    added

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -2,16 +2,17 @@
 const square = require('./5-square');
 
 class Square extends square {
-  constructor(size) {
+  constructor (size) {
     super(size);
     this.size = size;
   }
+
   charPrint (c) {
     if (typeof c === 'undefined') {
       super.print();
     } else if (c === 'C') {
       for (let i = 0; i < this.size; i++) {
-          console.log('C'.repeat(this.size));
+        console.log('C'.repeat(this.size));
       }
     }
   }
