0f1b8361d29de553443ea6074a65ee0617d1c005
BruceIsBat <awoteduibrahim@gmail.com>
2023-11-18 08:30:22 -0800

    converter to base 10 and 16

---
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+exports.converter = function (base) {
+  return function convertNumber(number) {
+    if (number < base) {
+      return number.toString(base);
+    } else {
+      return convertNumber(Math.floor(number / base)) + (number % base).toString(base);
+    }
+  };
+};
+
