de4dc4962c02b83360d81a9aecfbe7c7e7aaffb2
BruceIsBat <awoteduibrahim@gmail.com>
2024-04-29 12:29:03 +0100

    updated

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,20 +1,29 @@
 #!/usr/bin/python3
 """script that lists all states from the database hbtn_0e_0_usa
 """
+
+
 import MySQLdb
 import sys
 
 
-try:
-    db = MySQLdb._mysql.connect(host="localhost",
+db = MySQLdb.connect(host="localhost",
                                 port=3306,
                                 user=sys.argv[1],
                                 password=sys.argv[2],
                                 database=sys.argv[3])
+try:
+    db = MySQLdb.connect(host="localhost",
+                                port=3306,
+                                user=sys.argv[1],
+                                password=sys.argv[2],
+                                database=sys.argv[3])
+    cursor = db.cursor()
 
-    db.query("""SELECT * FROM states ORDER BY states.id ASC""")
-    r = db.store_result()
-    states = r.fetch_row(0, 0)
+    cursor.execute("""SELECT * FROM states ORDER BY states.id ASC""")
+
+    states = cursor.fetchall()
+
     for state in states:
         print((int(state[0].decode('utf-8')), state[1].decode('utf-8')))
 except MySQLdb.Error as e:
